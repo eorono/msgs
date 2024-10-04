fef6cbec79f118da6fec6f0da88f82267d1f264e
Eleazar <eorono@gmail.com>
2024-10-04 06:56:24 -0400

    first commit

---
+++ config/platforms.php
@@ -0,0 +1,9 @@
+
+<?php
+
+return [
+    'discord' => \App\Services\DiscordService::class,
+    'slack' => \App\Services\SlackService::class,
+    'telegram' => \App\Services\TelegramService::class,
+    'whatsapp' => \App\Services\WhatsappService::class,
+];
